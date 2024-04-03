3cd8394111b89593bf606426748a04dc9c436261
Adnan <adnanvandyck@gmail.com>
2024-04-03 15:00:47 -0400

    added functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleDark, toggleLight } from './features/modeSlice';
 
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const dispatch = useDispatch()
+
+  const toggleSwitch = () => {
+    mode.darkmode
+    ? dispatch(toggleLight())
+    : dispatch(toggleDark())
+  }
   
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'black' }} className="App">
       <Nav />
+      <button onClick={toggleSwitch}>Toggle Me</button>
       <ContentWrapper />
       <Footer />
     </div>
